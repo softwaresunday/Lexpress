51c37ae1e30ab422d93b960f7ad40c2d6a13f81f
Peter Reintjes <peter.reintjes@ncmls.org>
2011-02-19 01:15:30 -0500

    first commit

---
+++ doc/filt.pl
@@ -0,0 +1,17 @@
+#!C:/Perl/bin/perl.exe -w
+
+my $outfile = $ARGV[0];
+
+$outfile =~ s/\.pl$/.html/;
+
+open IN,"<$ARGV[0]";
+open OUT,">$outfile";
+while(<IN>)
+  {
+    s/^%//g;
+    s/<code>/<font size=4><b><pre>/g;
+    s/<\/code>/<\/pre><\/b><\/font>/g;
+    print OUT $_;
+  }
+close IN;
+close OUT;
